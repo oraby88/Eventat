b8718a6dd4f4d3aa2e445ec2ce80a4aa24b8713a
Mostafa Mohamed Oraby <mostafa.oraby159@gmail.com>
2024-10-14 10:39:50 +0300

    all changes

---
+++ src/app/components/qrcodescanner/qrcodescanner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qrcodescanner',
+  standalone: true,
+  imports: [],
+  templateUrl: './qrcodescanner.component.html',
+  styleUrl: './qrcodescanner.component.css'
+})
+export class QrcodescannerComponent {
+
+}
